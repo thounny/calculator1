9485d97b05a0a402297d4461c8f4816145355a9b
thounny <thounnykeo@gmail.com>
2021-11-22 22:58:07 -0500

    calculate num1 + num2 into Number

--- calculator.js
+++ calculator.js
@@ -17,7 +17,13 @@ app.get('/', function(req, res) {
 });
 
 app.post("/", function(req, res) {
-    res.send("Thanks for posting that 😎");
+
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+
+    var result = num1 + num2;
+
+    res.send("Calculating... " + result + " 😎");
 });
 
 app.listen(3000, function() {
