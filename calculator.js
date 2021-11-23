62fd6004841430c3603357dccf5d4c370c18afe6
thounny <thounnykeo@gmail.com>
2021-11-22 22:42:20 -0500

    npm i serve-favicon, app.post = post to home route

--- calculator.js
+++ calculator.js
@@ -1,10 +1,20 @@
 //jshint esversion:6
 
 const express = require('express');
+
 const app = express();
+
+var favicon = require('serve-favicon');
+var path = require('path');
+
+app.use(favicon(path.join(__dirname, 'favicon.ico')));
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+    res.send("Thanks for posting that 😎");
 });
 
 app.listen(3000, function() {
